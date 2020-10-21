72508d3464ce16599e193342b3365a4fbfd6f32d
Yerlyn Guerrero León <guerreroyerlyn221113@gmail.com>
2020-10-21 17:39:52 -0600

    Estructura

--- src/main.py
+++ src/main.py
@@ -1,1 +1,26 @@
-print("hola")
+
+
+def esPalabraReservada():
+    # Retorna verdadero o falso
+    #7 o más if
+    return
+
+def leerArchivo():
+    # leer archivo, lo divide (.split(" ")) cada división la manda a procesar(linea)
+
+    return
+
+def procesarInstruccion():
+    # llama esPalabraReservada()
+       # esPalabraReservada() is true { #Todos los if }
+       # esPalabraReservada() is false { mostrar mensaje error y seguir con la siguiente instrucción }
+
+    #Se añade a una lista de listas el tipo de complemento lexico, el texto y el numero de linea
+    #Ejm: [[PalabraReservada, lizano, 1], [Nombre, i, 1]....]
+    #Llamar a imprimir(<"Tipo de componente léxico", "Texto del componente léxico", “Atributos adicionales del componente”>)
+    return
+
+def main():
+    # Se saca del args
+    #leer archivo()
+    return
