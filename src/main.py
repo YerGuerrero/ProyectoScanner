bb1b6cf4fa926d40ab966da1842807d9e4e78665
Yerlyn Guerrero León <guerreroyerlyn221113@gmail.com>
2020-10-23 12:36:40 -0600

    Se hace el archivo orientado a objetos

--- src/main.py
+++ src/main.py
@@ -1,76 +1,80 @@
 import sys
 
-def esPalabraReservada(palabra):
-    if (palabra=="platillo"):
-        return True
-    elif(palabra=="sal"):
-        return True
-    elif (palabra == "lizano"):
-        return True
-    elif (palabra == "("):
-        return True
-    elif (palabra == "=>"):
-        return True
-    elif (palabra == ")"):
-        return True
-    elif (palabra == ","):
-        return True
-    elif (palabra == "["):
-        return True
-    elif (palabra == "]"):
-        return True
-    elif (palabra == "-"):
-        return True
-    elif (palabra == "mezclar"):
-        return True
-    elif (palabra == "devolver"):
-        return True
-    elif (palabra == "fresco"):
-        return True
-    elif (palabra == "agua_dulce"):
-        return True
-    elif (palabra == "cafe"):
-        return True
-    elif (palabra == "listo"):
-        return True
-    elif (palabra == "AND"):
-        return True
-    elif (palabra == "OR"):
-        return True
-    elif (palabra == "$"):
-        return True
-    elif (palabra == "menu"):
-        return True
-    elif (palabra == "nl"):
-        return True
-    elif (palabra == "tab"):
-        return True
-    elif (palabra == "acompanar"):
-        return True
-    elif (palabra == "melcochon"):
-        return True
-    elif (palabra == "servir"):
-        return True
-    elif (palabra == "tomarOrden"):
-        return True
-    elif (palabra == "cocinar"):
-        return True
-    else:
-        return False
+class Scanner:
+    def __init__(self, palabra):
+        self.palabra = palabra
 
-def leerArchivo():
-    # leer archivo, lo divide (.split(" ")) cada division la manda a procesar(linea)
-    return
+    def esPalabraReservada(self):
+        if (self.palabra=="platillo"):
+            return True
+        elif(self.palabra=="sal"):
+            return True
+        elif (self.palabra == "lizano"):
+            return True
+        elif (self.palabra == "("):
+            return True
+        elif (self.palabra == "=>"):
+            return True
+        elif (self.palabra == ")"):
+            return True
+        elif (self.palabra == ","):
+            return True
+        elif (self.palabra == "["):
+            return True
+        elif (self.palabra == "]"):
+            return True
+        elif (self.palabra == "-"):
+            return True
+        elif (self.palabra == "mezclar"):
+            return True
+        elif (self.palabra == "devolver"):
+            return True
+        elif (self.palabra == "fresco"):
+            return True
+        elif (self.palabra == "agua_dulce"):
+            return True
+        elif (self.palabra == "cafe"):
+            return True
+        elif (self.palabra == "listo"):
+            return True
+        elif (self.palabra == "AND"):
+            return True
+        elif (self.palabra == "OR"):
+            return True
+        elif (self.palabra == "$"):
+            return True
+        elif (self.palabra == "menu"):
+            return True
+        elif (self.palabra == "nl"):
+            return True
+        elif (self.palabra == "tab"):
+            return True
+        elif (self.palabra == "acompanar"):
+            return True
+        elif (self.palabra == "melcochon"):
+            return True
+        elif (self.palabra == "servir"):
+            return True
+        elif (self.palabra == "tomarOrden"):
+            return True
+        elif (self.palabra == "cocinar"):
+            return True
+        else:
+            return False
 
-def procesarInstruccion():
-    # llama esPalabraReservada()
-    # esPalabraReservada() is true { #Todos los if }
-    # esPalabraReservada() is false { mostrar mensaje error y seguir con la siguiente instruccion }
+    def leerArchivo(self):
+        # leer archivo, lo divide (.split(" ")) cada division la manda a procesar(linea)
+        return
 
-    # Se anade a una lista de listas el tipo de complemento lexico, el texto y el numero de linea
-    # Ejm: [[PalabraReservada, lizano, 1], [Nombre, i, 1]....]
-    # Llamar a imprimir Tipo de componente lexico, Texto del componente lexico, Atributos adicionales del componente
-    return
+    def procesarInstruccion(self):
+        # llama esPalabraReservada()
+        # esPalabraReservada() is true { #Todos los if }
+        # esPalabraReservada() is false { mostrar mensaje error y seguir con la siguiente instruccion }
+
+        # Se anade a una lista de listas el tipo de complemento lexico, el texto y el numero de linea
+        # Ejm: [[PalabraReservada, lizano, 1], [Nombre, i, 1]....]
+        # Llamar a imprimir Tipo de componente lexico, Texto del componente lexico, Atributos adicionales del componente
+        return
 
 
 def main():
